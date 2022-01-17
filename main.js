fc7337f51f66593545c0cdde387308d13dc00564
naaru2011 <83050539+naaru2011@users.noreply.github.com>
2022-01-17 14:32:30 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+//https://teachablemachine.withgoogle.com/models/akhOEZEOx/
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src="' + data_uri+'">';
+    });
+}
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/akhOEZEOx/model.json', modelLoaded);
+console.log('ml5 version:', ml5.version);
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+function check() {
+    img = document.getElementById("captured_image");
+
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+if (error != null) {
+    console.log(error);
+}    
+else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_acurracy").innerHTML = results[0].confidence.toFixed(1);
+
+}   
+}
